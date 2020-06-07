b7376f7f0b46de5d2d681a1a85c02bbeb6e86644
Vincent Riouallon <riouallonvincent@gmail.com>
2020-06-07 21:00:00 +0200

    chore: rm trailing nb of char

--- postInstall.sh
+++ postInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://pkgbuild.com/~eschwartz/repo/x86_64-extracted/pacman-static 48
+wget https://pkgbuild.com/~eschwartz/repo/x86_64-extracted/pacman-static
 chmod +x pacman-static
 ./pacman-static -Syyu
 pacman -S python
